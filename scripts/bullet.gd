880059c725e3095c1bfd78a84a5ef3bfba5ecff5
Rafael Carneiro <rafaeldelima97@hotmail.com>
2023-04-11 04:26:30 -0300

    bug fixes, clone machine implementada e clone type 0 iniciado

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,23 +1,32 @@
 extends Area2D
 
-var vel = 800
-var direction = Vector2()
-
-var BULLET_DAMAGE := 1
+var bullet_velocity := 100
+var bullet_direction := Vector2()
+var bullet_damage := 1.0
 
 func _physics_process(delta):
-	translate(direction * vel * delta)
+	translate(bullet_direction * bullet_velocity * delta)
 
-func setDirection(dir) -> void:
-	self.direction = dir
+func setDirection(direction) -> void:
+	self.bullet_direction = direction
 
-func setVel(bulletSpeed) -> void:
-	vel = bulletSpeed
+func setVelocity(velocity) -> void:
+	self.bullet_velocity = velocity
+	
+func setBulletDamage(damage) -> void:
+	self.bullet_damage = damage
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	self.queue_free()
 
 func _on_body_entered(body):
 	if body.name == "CloneMachine":
-		body.applyDamage(BULLET_DAMAGE)
-	queue_free()
+		body.applyDamage(bullet_damage)
+	self.queue_free()
+
+func _on_area_entered(area):
+	if area.is_in_group("enemy"):
+		area.applyDamage(bullet_damage)
+		self.queue_free()
+
+#	self.queue_free()
---
+++ scripts/clone_type_0.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+var enemy_velocity := 300
+var enemy_health := 5.0
+var direction := Vector2(-1, 0)
+@onready var walkAnim = $WalkAnim
+@onready var destroyAnim = $DestroyAnim
+
+func _process(delta):
+	self.attack(delta)
+
+func attack(delta):
+	walkAnim.play("WalkCloneType0")
+	translate(direction * enemy_velocity * delta)
+
+func applyDamage(bullet_damage):
+	self.enemy_health -= bullet_damage
+
+	if not enemy_health:
+		self.destroyObject()
+	
+func destroyObject():
+	self.collision_layer = 0
+	destroyAnim.play("DestroyCloneType0")
+	await destroyAnim.animation_finished
+	self.queue_free()
+
+func _on_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_index):
+	if body.name == "TileMap":
+		self.queue_free()
